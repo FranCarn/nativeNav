708cb463b1650da69cdb07ea6f5a62a068dcf48b
@FranCarn <carnevalefranco99@gmail.com>
2023-09-22 15:38:09 -0300

    icons initial setting

--- src/router/Tabs.tsx
+++ src/router/Tabs.tsx
@@ -3,26 +3,58 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {Tab1Screen, Tab2Screen} from '../screens';
 import {StackNavigator} from './StackNavigator';
 import {colors} from '../theme/appTheme';
+import {Text} from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
 export const Tabs = () => {
   return (
     <Tab.Navigator
-      screenOptions={{
+      screenOptions={({route}) => ({
+        tabBarIcon: ({color, size, focused}) => {
+          let iconName: string = '';
+          switch (route.name) {
+            case 'Tab1Screen':
+              iconName = 'T1';
+              break;
+            case 'Tab2Screen':
+              iconName = 'T2';
+              break;
+            case 'StackNavigator':
+              iconName = 'ST';
+              break;
+          }
+
+          return <Text style={{color, width: size}}>{iconName}</Text>;
+        },
         tabBarActiveTintColor: colors.primary,
         tabBarStyle: {
           borderTopColor: colors.primary,
           borderTopWidth: 5,
           elevation: 0,
         },
-      }}
+        tabBarLabelStyle: {
+          fontSize: 13,
+        },
+      })}
       sceneContainerStyle={{
         backgroundColor: '#fff',
       }}>
+      {/* <Tab.Screen
+        name="Tab1Screen"
+        options={{
+          title: 'Tab 1',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+        component={Tab1Screen}
+      /> */}
       <Tab.Screen
         name="Tab1Screen"
-        options={{title: 'Tab 1'}}
+        options={{
+          title: 'Tab 1',
+        }}
         component={Tab1Screen}
       />
       <Tab.Screen
