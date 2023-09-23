f7ea3016977a9f8645cdba3a6a6f25bf7818adcb
@FranCarn <carnevalefranco99@gmail.com>
2023-09-23 13:30:19 -0300

    new styles on material top tab navigator

--- src/router/TopTabNavigator.tsx
+++ src/router/TopTabNavigator.tsx
@@ -1,12 +1,47 @@
 import React from 'react';
+import {Text} from 'react-native';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import {AlbumsScreen, ChatScreen, ContactsScreen} from '../screens';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {colors} from '../theme/appTheme';
 
 const Tab = createMaterialTopTabNavigator();
 
 export const TopTabNavigator = () => {
+  const {top: paddingTop} = useSafeAreaInsets();
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      style={{paddingTop}}
+      sceneContainerStyle={{
+        backgroundColor: '#fff',
+      }}
+      screenOptions={({route}) => ({
+        tabBarIcon: ({color, focused}) => {
+          let iconName: string = '';
+          switch (route.name) {
+            case 'Chat':
+              iconName = 'CH';
+              break;
+            case 'Contacts':
+              iconName = 'CO';
+              break;
+            case 'Albums':
+              iconName = 'AL';
+              break;
+          }
+
+          return <Text style={{color}}>{iconName}</Text>;
+        },
+        tabBarPressColor: colors.primary,
+        tabBarShowIcon: true,
+        tabBarIndicatorStyle: {
+          backgroundColor: colors.primary,
+        },
+        tabBarStyle: {
+          shadowColor: 'transparent',
+          elevation: 0,
+        },
+      })}>
       <Tab.Screen name="Chat" component={ChatScreen} />
       <Tab.Screen name="Contacts" component={ContactsScreen} />
       <Tab.Screen name="Albums" component={AlbumsScreen} />
