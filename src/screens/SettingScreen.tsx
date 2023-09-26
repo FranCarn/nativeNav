38e7ec5ba6960d18982236514d3ff3ccfbc75093
@FranCarn <carnevalefranco99@gmail.com>
2023-09-25 21:07:50 -0300

    consuming context

--- src/screens/SettingScreen.tsx
+++ src/screens/SettingScreen.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {View, Text} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {styles} from '../theme/appTheme';
+import {AuthContext} from '../context';
 
 export const SettingScreen = () => {
   const {top} = useSafeAreaInsets();
+  const {authState} = useContext(AuthContext);
   return (
     <View style={{...styles.globalMargin, marginTop: top}}>
       <Text style={styles.title}>Settings Screen</Text>
+      <Text>{JSON.stringify(authState, null, 3)}</Text>
     </View>
   );
 };
